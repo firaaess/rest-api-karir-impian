f11b137c8985a72b1270a709c4e9bbf93d9c7777
firaaess <plantongaming24@gmail.com>
2024-10-30 09:00:20 +0700

    default code

--- controllers/company.controllers.js
+++ controllers/company.controllers.js
@@ -143,6 +143,27 @@ export const getCompany = async (req, res) => {
     }
 };
 
+export const getCompanyById = async (req, res) => {
+    try {
+        const companyId = req.params.id
+        const company = await Company.findById(companyId)
+
+        if (!company) {
+            return res.status(404).json({
+                message: 'Perusahaan tidak di temukan',
+                success: false
+            })
+        }
+
+        return res.status(200).json({
+            company,
+            success : true
+        })
+    } catch (error) {
+        console.error(error)
+    }
+}
+
 export const updateCompany = async (req, res) => {
     try {
         const { name, description, website, location } = req.body;
--- routes/company.routes.js
+++ routes/company.routes.js
@@ -1,12 +1,13 @@
 import express from 'express'
 import isAuthenticated from '../middlewares/isAuthenticated.js'
-import { addCompany, approveCompany, deleteCompany, getCompany, updateCompany } from '../controllers/company.controllers.js'
+import { addCompany, approveCompany, deleteCompany, getCompany, getCompanyById, updateCompany } from '../controllers/company.controllers.js'
 import { singleUpload } from '../middlewares/multer.js'
 
 const router = express.Router()
 
 router.route('/add').post(isAuthenticated ,addCompany)
 router.route('/get').get(isAuthenticated,getCompany)
+router.route('/get/:id').get(isAuthenticated,getCompanyById)
 router.route('/:id/approve').put(isAuthenticated,approveCompany)
 router.route('/update/:id').put(isAuthenticated,singleUpload,updateCompany)
 router.route('/delete/:id').delete(isAuthenticated, deleteCompany)
